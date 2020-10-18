20c998b3c6d215826d9ad5b35abdf1cff804b914
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 17:45:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -7,10 +7,17 @@ function forLoop(array) {
 }
 
 function whileLoop(n) {
-  while(n != 0) {
+  while(n !== 0) {
     n--;
     console.log(n);
   }
 
   return 'done';
+}
+
+function doWhileLoop(num) {
+  do {
+    n--;
+    console.log('I run once regardless.');
+  } while (n !== 0)
 }
